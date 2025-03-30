6671acab762b9757d68885761c9364b251d4d264
lloeffel <liamloeffel@gmail.com>
2025-03-29 23:03:57 -0400

    color changes

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -3,12 +3,9 @@ function ColorMyPencils(color)
 	color = color or "onedark"
 	vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-	vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
-	vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalNC", { bg = "black" })
-	vim.api.nvim_set_hl(0, "MsgArea", { bg = "black" })
+    vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "white"})
+    vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "white"})
+    vim.api.nvim_set_hl(0, "LineNr", { fg = "Magenta"})
 end
 
 ColorMyPencils()
