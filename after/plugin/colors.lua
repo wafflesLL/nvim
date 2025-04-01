9bd9cf58de5a423da3e5fe274dd33584702e53fe
lloeffel <liamloeffel@gmail.com>
2025-04-01 10:37:01 -0400

    adding diagnostic messages, made changing colorscheme funcs easier

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,4 +1,6 @@
-function gruvbox()
+
+function Gruvbox(isTransparent)
+    isTransparent = isTransparent or false
 	require("gruvbox").setup({
         terminal_colors = true, -- add neovim terminal colors
         undercurl = true,
@@ -21,24 +23,23 @@ function gruvbox()
         palette_overrides = {},
         overrides = {},
         dim_inactive = false,
-        transparent_mode = false,
+        transparent_mode = isTransparent,
       })
       vim.cmd("colorscheme gruvbox")
 end
 
-function onedark()
+function Onedark(isTransparent)
+    isTransparent = isTransparent or false
 	require('onedark').setup  {
         -- Main options --
         style = 'dark', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-        transparent = false,  -- Show/hide background
+        transparent = isTransparent,  -- Show/hide background
         term_colors = true, -- Change terminal color as per the selected theme style
         ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
         cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
-    
         -- toggle theme style ---
         toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
         toggle_style_list = {'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'}, -- List of styles to toggle between
-    
         -- Change code style ---
         -- Options are italic, bold, underline, none
         -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
@@ -49,16 +50,13 @@ function onedark()
             strings = 'none',
             variables = 'none'
         },
-    
         -- Lualine options --
         lualine = {
             transparent = false, -- lualine center bar transparency
         },
-    
         -- Custom Highlights --
         colors = {}, -- Override default colors
-        highlights = {}, -- Override highlight groups
-    
+        highlights = {}, -- Override highlight groups 
         -- Plugins Config --
         diagnostics = {
             darker = true, -- darker colors for diagnostic
@@ -69,16 +67,17 @@ function onedark()
     vim.cmd.colorscheme("onedark")
 end
 
-function transparent()
-    vim.api.nvim_set_hl(0, "Normal", {bg = "none"});
-    vim.api.nvim_set_hl(0, "NormalNC", {bg = "none"});
-    vim.api.nvim_set_hl(0, "NormalFload", {bg = "none"});
-    vim.api.nvim_set_hl(0, "EndOfBuffer", {bg = "none"});
-    vim.api.nvim_set_hl(0, "MsgArea", {bg = "none"});
-    vim.api.nvim_set_hl(0, "SignColumn", {bg = "none"});
-    vim.api.nvim_set_hl(0, "", {bg = "none"});
+
+function SetCS(color,isTransparent)
+    color = color or 'onedark'
+    isTransparent = isTransparent or false;
+    if color == 'onedark' then
+        Onedark(isTransparent)
+    elseif color == 'gruvbox' then
+        Gruvbox(isTransparent)
+    else
+        Onedark(isTransparent)
+    end
 end
 
-
-
-onedark()
+SetCS()
--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -100,3 +100,20 @@ cmp.setup({
 	}),
 })
 
+vim.diagnostic.config({
+    virtual_text = false;
+    signs = true,
+    update_in_insert = false,
+    underline = true,
+    float = {
+        border = "rounded",
+        source = "always",
+    },
+})
+
+vim.api.nvim_create_autocmd("CursorHold", {
+  pattern = "*",
+  callback = function()
+    vim.diagnostic.open_float(nil, { focusable = false })
+  end,
+})
