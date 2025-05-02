af2e4de4b2638f67684623576dd3c5d446e2b3db
lloeffel <liamloeffel@gmail.com>
2025-05-01 23:14:18 -0400

    actually fixed treesitter this time

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -9,7 +9,7 @@ return {
       -- A list of parser names, or "all" (the listed parsers MUST always be installed)
       ensure_installed = { "c", "cpp", "javascript", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
       -- Install parsers synchronously (only applied to `ensure_installed`)
-      sync_install = true,
+      sync_install = false,
       auto_install = true,
       highlight = {
         enable = true,  -- Enables syntax highlighting using Treesitter
