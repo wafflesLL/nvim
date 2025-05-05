ba79f29d19e6ae6259e737b48322b8fd1e9693a7
lloeffel <liamloeffel@gmail.com>
2025-05-05 12:24:41 -0400

    changed compeltion keymappings

--- lua/options.lua
+++ lua/options.lua
@@ -76,9 +76,9 @@ vim.keymap.set("n", "<C-a>", "gg<S-v>G")
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
@@ -98,6 +98,7 @@ vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
+vim.opt.mouse = ""
 
 vim.opt.updatetime = 50
 vim.opt.wildignore:append { '*/node-modules/*' }
--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -3,18 +3,18 @@ return {
     version = '1.*',
     dependencies = { 'L3MON4D3/LuaSnip', version = 'v2.*' },
     opts = {
+        enabled = function() return not vim.tbl_contains({ "text", "markdown" }, vim.bo.filetype) end,
         keymap = {
             preset = 'default',
             ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
             ['<C-e>'] = { 'hide', 'fallback' },
-            ['<CR>'] = { 'accept', 'fallback' },
-            ['<S-Tab>'] = { 'select_prev', 'fallback' },
-            ['<Tab>'] = { 'select_next', 'fallback' },
+            ['<Tab>'] = { 'accept', 'fallback' },
+            ['<C-k>'] = { 'select_prev', 'fallback' },
+            ['<C-j>'] = { 'select_next', 'fallback' },
             ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
             ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
-            ['<C-k>'] = { 'scroll_documentation_up', 'fallback' },
-            ['<C-j>'] = { 'scroll_documentation_down', 'fallback' },
-            ['<C-d>'] = { 'show_documentation', 'fallback' },
+            ['<C-u>'] = { 'scroll_documentation_up', 'fallback' },
+            ['<C-d>'] = { 'scroll_documentation_down', 'fallback' },
         },
         appearance = {
             nerd_font_variant = 'mono'
